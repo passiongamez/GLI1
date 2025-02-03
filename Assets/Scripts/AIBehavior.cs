fa1a6daa0c0c48af56691d9b884432a2bb366bf5
Felix <felix.m.quinones777@gmail.com>
2025-02-03 11:26:10 -0600

    ai movement created

--- Assets/Scripts/AIBehavior.cs
+++ Assets/Scripts/AIBehavior.cs
@@ -1,17 +1,39 @@
+using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AIBehavior : MonoBehaviour
 {
     [SerializeField] Transform _startPos;
     [SerializeField] Transform _endPos;
+    [SerializeField]
+    Transform[] _path;
+    [SerializeField] int _currentPath = 0;
+    NavMeshAgent _agent;
 
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        if (_agent == null)
+        {
+            Debug.Log("Agent is null");
+        }
     }
 
     void Update()
     {
-        
+        if (_currentPath <= _path.Length - 1)
+        {
+            SetPath();
+        }
+    }
+
+    void SetPath()
+    {
+        _agent.destination = _path[_currentPath].position;
+        if (_agent.remainingDistance < .5f)
+        {
+            _currentPath += 1;
+        }
     }
 }
