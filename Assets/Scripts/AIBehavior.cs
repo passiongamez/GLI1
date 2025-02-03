f424949e97d32fcd9437d77b5356168059702a4b
Felix <felix.m.quinones777@gmail.com>
2025-02-03 07:37:17 -0600

    establised start pos

---
+++ Assets/Scripts/AIBehavior.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AIBehavior : MonoBehaviour
+{
+    [SerializeField] Transform _startPos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
